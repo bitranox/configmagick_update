04ea6cdedb02e2e48de23519eb0e84e17f0159e1
bitranox <rnowotny1966@gmail.com>
2019-07-24 17:21:15 +0200

    refractor

--- configmagick_update/configmagick_update.py
+++ configmagick_update/configmagick_update.py
@@ -2,14 +2,9 @@
 import errno
 import json
 import logging
-import pathlib
 import subprocess
 import sys
 from typing import List
-
-# EXT
-import psutil   # type: ignore
-import requests
 
 # OWN
 from configmagick_bash import lib_bash
@@ -48,38 +43,14 @@ def pip_update(name_or_link: str, use_sudo: bool) -> bool:  # returns updated or
         updated = pip_update_from_pypy(pypy_package=name_or_link, use_sudo=use_sudo)
         return updated
     elif package_type == 'git_package':
-        if not is_pip_git_package_up_to_date(git_link=name_or_link):
-            pip_update_from_git(git_link=name_or_link)
-            return True
+        if not lib_helpers.is_pip_git_package_up_to_date(git_link=name_or_link):
+            updated = pip_update_from_git(git_link=name_or_link, use_sudo=use_sudo)     # returns always true, but we do it only when update is needed
+            return updated
     elif package_type == 'weblink':
-        pip_update_from_weblink(weblink=name_or_link)
+        pip_update_from_weblink(weblink=name_or_link, use_sudo=use_sudo)
         return True
     else:
         return False
-
-
-# depricated
-def is_pip_pypy_package_up_to_date(pypy_package: str, use_sudo: bool) -> bool:
-
-    pip_command = lib_helpers.get_latest_pip_command().command_string
-    ls_commands = lib_helpers.get_ls_commands_prepend_sudo([pip_command, 'list', '-o'], use_sudo=use_sudo)
-    shell_response = lib_shell.run_shell_ls_command(ls_commands)
-    result_lines = lib_regexp.reg_grep(pypy_package, shell_response.stdout)
-    if not result_lines:
-        return False
-    # there can be more grep matches - for instance package distro, distro-info
-    # would create two result lines
-    for line in result_lines:
-        package, version, latest, package_type = line.split()
-        if package == pypy_package:
-            if version != latest:
-                return False
-            else:
-                return True
-
-def is_pip_git_package_up_to_date(git_link: str) -> bool:
-
-    return None
 
 
 def pip_update_from_pypy(pypy_package: str, use_sudo: bool, show_output: bool = True) -> bool:
@@ -90,6 +61,7 @@ def pip_update_from_pypy(pypy_package: str, use_sudo: bool, show_output: bool = 
     >>> assert pip_update_from_pypy('urllib3==1.24.1', use_sudo=True, show_output=False) is not None    # first Update to old Version
     >>> assert pip_update_from_pypy('urllib3', use_sudo=True, show_output=False) == True                # second Update to new Version
     >>> assert pip_update_from_pypy('urllib3', use_sudo=True, show_output=False) == False               # third Update - is already up to date
+    >>> assert pip_update_from_pypy('chardet', use_sudo=True, show_output=False) is not None            # third Update - is already up to date
 
     """
     try:
@@ -109,144 +81,38 @@ def pip_update_from_pypy(pypy_package: str, use_sudo: bool, show_output: bool = 
             raise ValueError(error)
 
 
-def pip_update_from_weblink(weblink: str, show_output: bool = True) -> bool:
+def pip_update_from_git(git_link: str, use_sudo: bool, show_output: bool = True) -> bool:
     """
-    >>> import unittest
-    >>> assert pip_update_from_weblink('pip', show_output=False) == True
-    >>> unittest.TestCase().assertRaises(ValueError, pip_update_from_weblink, 'https://some_unknown_link/package.zip', show_output=False)
-    """
-    return pip_update_from_pypy(pypy_package=weblink, show_output=show_output)
+    :returns updated - True if updated, False if it was already up to date
 
-
-def get_git_repository_slug_from_link(git_link: str) -> str:
-    """
-    >>> import unittest
-    >>> assert get_git_repository_slug_from_link('https://github.com/pypa/pip.git') == 'pypa/pip'
-    >>> assert get_git_repository_slug_from_link('git+https://github.com/pypa/pip.git') == 'pypa/pip'
-    >>> assert get_git_repository_slug_from_link('https://github.com/pypa/pip/archive/master.zip') == 'pypa/pip'
-    >>> unittest.TestCase().assertRaises(ValueError, get_git_repository_slug_from_link, 'https://some_unknown_link/master.zip')
-    >>> unittest.TestCase().assertRaises(ValueError, get_git_repository_slug_from_link, 'git+https:/github.com/pypa/pip.git')
+    >>> assert pip_update_from_git('git+https://github.com/pypa/pip.git', use_sudo=True, show_output=False) == True        # always true here
 
     """
 
-    # https://github.com/pypa/pip.git | git+https://github.com/pypa/pip.git | https://github.com/pypa/pip/archive/master.zip
-    git_repository_slug = ''
-    try:
-        git_repository_slug = git_link.split('https://github.com/')[1]   # pypa/pip.git | pypa/pip.git | pypa/archive/master.zip
-        git_repository_slug = git_repository_slug.rsplit('.', 1)[0]                 # pypa/pip | pypa/pip | pypa/archive/master
-        git_repository_slug = '/'.join(git_repository_slug.split('/')[:2])         # bitranox/lib_path | bitranox/lib_doctest_pycharm
-        elements = git_repository_slug.split('/')
-
-        if len(elements) != 2 or len(elements[0]) == 0 or len(elements[1]) == 0:
-            error = 'can not get the repository slug "{git_repository_slug}" from link "{git_link}" - wrong link ?'\
-                .format(git_repository_slug=git_repository_slug, git_link=git_link)
-
-            lib_bash.banner(logging.ERROR, error)
-            raise ValueError(error)
-
-        if '.zip' in git_link.lower():
-            sanitized_git_link = get_sanitized_git_link(git_repository_slug=git_repository_slug)
-            warning = 'better use "{sanitized_git_link}" than "{git_link} unless You need it for a reason"'\
-                .format(sanitized_git_link=sanitized_git_link, git_link=git_link)
-            lib_bash.banner(logging.WARNING, warning)
-
-        return git_repository_slug
-    except Exception:
-        error = 'can not get the repository slug "{git_repository_slug}" from link "{git_link}" - wrong link ?'\
-                        .format(git_repository_slug=git_repository_slug, git_link=git_link)
-
-        lib_bash.banner(logging.ERROR, error)
-        raise ValueError(error)
+    updated = pip_update_from_pypy(pypy_package=git_link, use_sudo=use_sudo)
+    git_repository_slug = lib_helpers.get_git_repository_slug_from_link(git_link=git_link)
+    git_remote_hash = lib_helpers.get_git_remote_hash(git_repository_slug=git_repository_slug)
+    lib_helpers.save_git_hash_to_database(key=git_link, git_hash=git_remote_hash)
+    return updated
 
 
-def get_git_remote_hash(git_repository_slug: str) -> str:
+def pip_update_from_weblink(weblink: str, use_sudo: bool, show_output: bool = True) -> bool:
+    return pip_update_from_pypy(pypy_package=weblink, use_sudo=use_sudo, show_output=show_output)
+
+
+def is_pip_git_package_up_to_date(git_link: str) -> bool:
     """
-    >>> # https://github.com/pypa/pip.git | git+https://github.com/pypa/pip.git | https://github.com/pypa/archive/master.zip
-
-    >>> git_repository_slug = get_git_repository_slug_from_link(git_link='https://github.com/pypa/pip.git')
-    >>> assert len(get_git_remote_hash(git_repository_slug=git_repository_slug)) == len('59e6ce2847bda24b3f29683251d10ae5c3cab357')
-    >>> git_repository_slug = get_git_repository_slug_from_link(git_link='git+https://github.com/pypa/pip.git')
-    >>> assert len(get_git_remote_hash(git_repository_slug=git_repository_slug)) == len('59e6ce2847bda24b3f29683251d10ae5c3cab357')
-    >>> git_repository_slug = get_git_repository_slug_from_link(git_link='https://github.com/pypa/archive/master.zip')
-    >>> assert len(get_git_remote_hash(git_repository_slug=git_repository_slug)) == len('59e6ce2847bda24b3f29683251d10ae5c3cab357')
-
+    >>> assert pip_update_from_git('git+https://github.com/pypa/pip.git', use_sudo=True, show_output=False) == True        # always true here
+    >>> assert(is_pip_git_package_up_to_date('git+https://github.com/pypa/pip.git')) == True
 
     """
-    url = 'https://github.com/{git_repository_slug}.git'.format(git_repository_slug=git_repository_slug)
-    git_command = lib_bash.get_bash_command('git')
-    result = lib_shell.run_shell_ls_command([git_command.command_string, '--no-pager', 'ls-remote', '--quiet'])
-    line = lib_regexp.reg_grep('HEAD', result.stdout)[0].expandtabs()
-    git_remote_hash = line.split()[0]
-    return git_remote_hash
-
-
-def get_git_local_hash_from_directory(path_install_directory: str) -> str:
-    # bash: git_local_hash=$(cat /usr/local/lib_bash/.git/refs/heads/master)
-    logger.warning('STUB get_git_local_hash_from_directory')
-    return ''
-
-
-def get_git_local_hash_from_database_or_blank(key: str) -> str:
-    """
-    >>> save_path_version_file = Config.path_version_file
-    >>> Config.path_version_file = Config.path_version_files_dir / 'test_database'
-    >>> if Config.path_version_file.exists(): Config.path_version_file.unlink()
-    >>> data_dict=dict()
-    >>> save_git_hash_to_database('a','A')
-    >>> save_git_hash_to_database('b','B')
-    >>> get_git_local_hash_from_database_or_blank('a')
-    >>> get_git_local_hash_from_database_or_blank('b')
-    >>> get_git_local_hash_from_database_or_blank('c')
-    >>> # Config.path_version_file.unlink()
-    >>> Config.path_version_file = save_path_version_file
-
-    """
-
-    if not Config.path_version_file.exists():
-        return ''
-
-    with open(str(Config.path_version_file), 'r') as f:
-        local_git_hash_hashed_by_key = json.load(f)
-
-    local_hash_from_database = local_git_hash_hashed_by_key.get(key, '')    # '' if not found
-    return local_hash_from_database
-
-
-def save_git_hash_to_database(key: str, git_hash: str) -> bool:
-
-    if Config.path_version_file.exists():
-        with open(str(Config.path_version_file), 'r') as f:
-            local_git_hash_hashed_by_key = json.load(f)
-    else:
-        local_git_hash_hashed_by_key = dict()
-
-    local_git_hash_hashed_by_key[key] = git_hash
-    with open(str(Config.path_version_file), 'w') as f:
-        json.dump(local_git_hash_hashed_by_key, f)
-    # lib_bash.fix_ownership(user=,fileobject=)  # TODO
-    # lib_bash.fix_permissions(user=, fileobject=, recursive=True) # TODO
-    return True
-
-
-def get_sanitized_git_link(git_repository_slug: str) -> str:
-    """
-    >>> assert get_sanitized_git_link(git_repository_slug='pypa/pip') == 'git+https://github.com/pypa/pip.git'
-    """
-
-    sanitized_git_link = 'git+https://github.com/{git_repository_slug}.git'.format(git_repository_slug=git_repository_slug)
-    return sanitized_git_link
-
-
-def pip_update_from_git(git_link: str) -> bool:
-    git_repository_slug = get_git_repository_slug_from_link(git_link=git_link)
-    git_remote_hash = get_git_remote_hash(git_repository_slug=git_repository_slug)
-    git_local_hash = get_git_local_hash_from_database_or_blank(key=git_link)
+    git_repository_slug = lib_helpers.get_git_repository_slug_from_link(git_link=git_link)
+    git_remote_hash = lib_helpers.get_git_remote_hash(git_repository_slug=git_repository_slug)
+    git_local_hash = lib_helpers.get_git_local_hash_from_database_or_blank(key=git_link)
     if git_remote_hash != git_local_hash:
-        pip_update_from_pypy(pypy_package=git_link)
-    save_git_hash_to_database(key=git_link, git_hash=git_remote_hash)
-    return True
-
-
+        return False
+    else:
+        return True
 
 
 def main(sys_argv: List[str] = sys.argv[1:]) -> None:
@@ -278,6 +144,7 @@ def main(sys_argv: List[str] = sys.argv[1:]) -> None:
         sys.exit(errno.EINVAL)      # pragma: no cover
     except Exception:
         sys.exit(1)                 # pragma: no cover
+
 
 
 if __name__ == '__main__':
--- configmagick_update/lib_helpers.py
+++ configmagick_update/lib_helpers.py
@@ -1,8 +1,18 @@
 # STDLIB
+import json
+import logging
 from typing import List
 
 # OWN
-import lib_bash
+from configmagick_bash import lib_bash
+import lib_regexp
+import lib_shell
+
+# PROJ
+from config import Config
+
+
+logger = logging.getLogger()
 
 
 def get_package_type(name_or_link: str) -> str:
@@ -90,3 +100,121 @@ def get_pypy_package_name_without_version(pypy_package: str) -> str:
     pypy_package = pypy_package.split('>')[0]
     pypy_package = pypy_package.split('<')[0]
     return pypy_package
+
+
+def get_git_repository_slug_from_link(git_link: str) -> str:
+    """
+    >>> import unittest
+    >>> assert get_git_repository_slug_from_link('https://github.com/pypa/pip.git') == 'pypa/pip'
+    >>> assert get_git_repository_slug_from_link('git+https://github.com/pypa/pip.git') == 'pypa/pip'
+    >>> assert get_git_repository_slug_from_link('https://github.com/pypa/pip/archive/master.zip') == 'pypa/pip'
+    >>> unittest.TestCase().assertRaises(ValueError, get_git_repository_slug_from_link, 'https://some_unknown_link/master.zip')
+    >>> unittest.TestCase().assertRaises(ValueError, get_git_repository_slug_from_link, 'git+https:/github.com/pypa/pip.git')
+
+    """
+
+    # https://github.com/pypa/pip.git | git+https://github.com/pypa/pip.git | https://github.com/pypa/pip/archive/master.zip
+    git_repository_slug = ''
+    try:
+        git_repository_slug = git_link.split('https://github.com/')[1]   # pypa/pip.git | pypa/pip.git | pypa/archive/master.zip
+        git_repository_slug = git_repository_slug.rsplit('.', 1)[0]                 # pypa/pip | pypa/pip | pypa/archive/master
+        git_repository_slug = '/'.join(git_repository_slug.split('/')[:2])         # bitranox/lib_path | bitranox/lib_doctest_pycharm
+        elements = git_repository_slug.split('/')
+
+        if len(elements) != 2 or len(elements[0]) == 0 or len(elements[1]) == 0:
+            error = 'can not get the repository slug "{git_repository_slug}" from link "{git_link}" - wrong link ?'\
+                .format(git_repository_slug=git_repository_slug, git_link=git_link)
+
+            lib_bash.banner(logging.ERROR, error)
+            raise ValueError(error)
+
+        if '.zip' in git_link.lower():
+            sanitized_git_link = get_sanitized_git_link(git_repository_slug=git_repository_slug)
+            warning = 'better use "{sanitized_git_link}" than "{git_link} unless You need it for a reason"'\
+                .format(sanitized_git_link=sanitized_git_link, git_link=git_link)
+            lib_bash.banner(logging.WARNING, warning)
+
+        return git_repository_slug
+    except Exception:
+        error = 'can not get the repository slug "{git_repository_slug}" from link "{git_link}" - wrong link ?'\
+                        .format(git_repository_slug=git_repository_slug, git_link=git_link)
+
+        lib_bash.banner(logging.ERROR, error)
+        raise ValueError(error)
+
+
+def get_git_remote_hash(git_repository_slug: str) -> str:
+    """
+    >>> # https://github.com/pypa/pip.git | git+https://github.com/pypa/pip.git | https://github.com/pypa/archive/master.zip
+
+    >>> git_repository_slug = get_git_repository_slug_from_link(git_link='https://github.com/pypa/pip.git')
+    >>> assert len(get_git_remote_hash(git_repository_slug=git_repository_slug)) == len('59e6ce2847bda24b3f29683251d10ae5c3cab357')
+    >>> git_repository_slug = get_git_repository_slug_from_link(git_link='git+https://github.com/pypa/pip.git')
+    >>> assert len(get_git_remote_hash(git_repository_slug=git_repository_slug)) == len('59e6ce2847bda24b3f29683251d10ae5c3cab357')
+    >>> git_repository_slug = get_git_repository_slug_from_link(git_link='https://github.com/pypa/pip/archive/master.zip')
+    >>> assert len(get_git_remote_hash(git_repository_slug=git_repository_slug)) == len('59e6ce2847bda24b3f29683251d10ae5c3cab357')
+
+    """
+    url = 'https://github.com/{git_repository_slug}.git'.format(git_repository_slug=git_repository_slug)
+    git_command = lib_bash.get_bash_command('git')
+    result = lib_shell.run_shell_ls_command([git_command.command_string, '--no-pager', 'ls-remote', '--quiet', url])
+    line = lib_regexp.reg_grep('HEAD', result.stdout)[0].expandtabs()
+    git_remote_hash = line.split()[0]
+    return git_remote_hash
+
+
+def get_git_local_hash_from_directory(path_install_directory: str) -> str:
+    # bash: git_local_hash=$(cat /usr/local/lib_bash/.git/refs/heads/master)
+    logger.warning('STUB get_git_local_hash_from_directory')
+    return ''
+
+
+def get_git_local_hash_from_database_or_blank(key: str) -> str:
+    """
+    >>> save_path_version_file = Config.path_version_file
+    >>> Config.path_version_file = Config.path_version_files_dir / 'test_database'
+    >>> if Config.path_version_file.exists(): Config.path_version_file.unlink()
+    >>> data_dict=dict()
+    >>> save_git_hash_to_database('a','A')
+    >>> save_git_hash_to_database('b','B')
+    >>> get_git_local_hash_from_database_or_blank('a')
+    >>> get_git_local_hash_from_database_or_blank('b')
+    >>> get_git_local_hash_from_database_or_blank('c')
+    >>> # Config.path_version_file.unlink()
+    >>> Config.path_version_file = save_path_version_file
+
+    """
+
+    if not Config.path_version_file.exists():
+        return ''
+
+    with open(str(Config.path_version_file), 'r') as f:
+        local_git_hash_hashed_by_key = json.load(f)
+
+    local_hash_from_database = local_git_hash_hashed_by_key.get(key, '')    # '' if not found
+    return local_hash_from_database
+
+
+def save_git_hash_to_database(key: str, git_hash: str) -> bool:
+
+    if Config.path_version_file.exists():
+        with open(str(Config.path_version_file), 'r') as f:
+            local_git_hash_hashed_by_key = json.load(f)
+    else:
+        local_git_hash_hashed_by_key = dict()
+
+    local_git_hash_hashed_by_key[key] = git_hash
+    with open(str(Config.path_version_file), 'w') as f:
+        json.dump(local_git_hash_hashed_by_key, f)
+    # lib_bash.fix_ownership(user=,fileobject=)  # TODO
+    # lib_bash.fix_permissions(user=, fileobject=, recursive=True) # TODO
+    return True
+
+
+def get_sanitized_git_link(git_repository_slug: str) -> str:
+    """
+    >>> assert get_sanitized_git_link(git_repository_slug='pypa/pip') == 'git+https://github.com/pypa/pip.git'
+    """
+
+    sanitized_git_link = 'git+https://github.com/{git_repository_slug}.git'.format(git_repository_slug=git_repository_slug)
+    return sanitized_git_link
