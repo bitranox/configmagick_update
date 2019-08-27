6b266afd9cd93d68bea8d67439d757930811ec38
bitranox <rnowotny1966@gmail.com>
2019-08-27 20:26:17 +0200

    refractor

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
 # this __init__.py is only meant for local package development
-from .configmagick_update import *
+try:
+    from .configmagick_update import *
+# this we need for pip install --install-option test
+except ImportError:
+    import configmagick_update
--- configmagick_update/__init__.py
+++ configmagick_update/__init__.py
@@ -1,5 +1,13 @@
+import pathlib
 from .configmagick_update import *
 
+
+def get_version() -> str:
+    with open(pathlib.Path(__file__).parent / 'version.txt', mode='r') as version_file:
+        version = version_file.readline()
+    return version
+
+
 __title__ = 'configmagick_update'
-__version__ = '0.0.1'
+__version__ = get_version()
 __name__ = 'configmagick_update'
